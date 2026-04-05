4d32134700f9473bce7b3381b2220ce1b9caa125
Schuyler Lewis <elvilewis40@gmail.com>
2026-04-05 22:15:39 +0530

    Update Color.kt

--- app/src/main/java/com/vianai/greenhouse/theme/Color.kt
+++ app/src/main/java/com/vianai/greenhouse/theme/Color.kt
@@ -1,11 +1,8 @@
 package com.vianai.greenhouse.theme
+
 import androidx.compose.ui.graphics.Color
 
 val VghCharcoal = Color(0xFF151619)
 val SpaceBlack = Color(0xFF050505)
 val SpaceBlue = Color(0xFF1E90FF)
 val DarkRed = Color(0xFF8B0000)
-val SurfaceDark = Color(0xFF1A1B1F)
-val TextPrimary = Color(0xFFE8E8E8)
-val TextSecondary = Color(0xFFA0A0A0)
-val Divider = Color(0xFF2A2B2F)
